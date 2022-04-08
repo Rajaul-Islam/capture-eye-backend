3e56d594537c8b83b64c2d5f64fcbd9788e14a34
rajaul rabbi <Rajaul-Islam>
2022-04-08 16:19:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+
+const products= require('./data/products')
+
+const app=express();
+
+const cors=require('cors')
+
+app.use(cors())
+ 
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+
+
+app.get('/',(req,res)=>{
+    res.send('from node js')
+})
+
+
+app.get('/products',(req,res)=>{
+    res.json(products);
+})
+app.get('/products/:id',(req,res)=>{
+    const product=products.find(p=>p.id==req.params.id);
+    res.json(product)
+})
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
